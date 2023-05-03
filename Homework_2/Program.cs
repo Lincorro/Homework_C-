2ebbb6915811766b30b6b6dee11cb5de27d0acfd
valmar98@mail.ru <valmar98@mail.ru>
2023-05-03 21:41:54 +0300

    add new Homework 6

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -1,29 +1,29 @@
-﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+﻿// //Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.Clear();
-Console.Write("Input first number ");
-string numbers = Console.ReadLine();
-int size = numbers.Length;
-int index = 0;
-int[] temp = new int[4];
-int max = 0;
-int scor = 0;
-int temp2 = 0;
-for (index = 0; index < size; index++)
-{
-    while(numbers[index] != ',')
-    {
-        temp[scor] = numbers[index];
-        scor++;
-        index++;
-    }
-     scor = 0;
-     index++;
-     temp2 = Convert.ToInt32(temp[] arg);
-     if ( temp2 > max)
-     {
-         max = temp2;
-     }
-} 
- System.Console.WriteLine(max);
+// Console.Clear();
+// Console.Write("Input first number ");
+// string numbers = Console.ReadLine();
+// int size = numbers.Length;
+// int index = 0;
+// int[] temp = new int[4];
+// int max = 0;
+// int scor = 0;
+// int temp2 = 0;
+// for (index = 0; index < size; index++)
+// {
+//     while(numbers[index] != ',')
+//     {
+//         temp[scor] = numbers[index];
+//         scor++;
+//         index++;
+//     }
+//      scor = 0;
+//      index++;
+//      temp2 = Convert.ToInt32(temp[] arg);
+//      if ( temp2 > max)
+//      {
+//          max = temp2;
+//      }
+// } 
+//  System.Console.WriteLine(max);
 
--- Homework_5/Program.cs
+++ Homework_5/Program.cs
@@ -1,53 +1,53 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿// // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
-int secondNumber(int num)
-{
-    if (num / 100 == 0)
-        return num % 10;
-    else
-        num = num / 10;
-       return num = num % 10;
-}
-System.Console.Write("Input  number typ (adc): ");
-int number = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine($"second number is -> {secondNumber(number)}");
+// // 456 -> 5
+// // 782 -> 8
+// // 918 -> 1
+// int secondNumber(int num)
+// {
+//     if (num / 100 == 0)
+//         return num % 10;
+//     else
+//         num = num / 10;
+//        return num = num % 10;
+// }
+// System.Console.Write("Input  number typ (adc): ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"second number is -> {secondNumber(number)}");
 
 
-// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
-int secondNumber(int num)
-{
-    if (num / 100 == 0)
-    {
-        System.Console.WriteLine("no third digit");
-        return num =-1;
-    }
-    else
-       return num = num % 10;
-}
-System.Console.Write("Input  number typ (adc): ");
-int number = Convert.ToInt32(Console.ReadLine());
+// // 645 -> 5
+// // 78 -> третьей цифры нет
+// // 32679 -> 6
+// int secondNumber(int num)
+// {
+//     if (num / 100 == 0)
+//     {
+//         System.Console.WriteLine("no third digit");
+//         return num =-1;
+//     }
+//     else
+//        return num = num % 10;
+// }
+// System.Console.Write("Input  number typ (adc): ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine($"second number is -> {secondNumber(number)}");
+// System.Console.WriteLine($"second number is -> {secondNumber(number)}");
 
-// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-// 6 -> да
-// 7 -> да
-// 1 -> нет
+// // 6 -> да
+// // 7 -> да
+// // 1 -> нет
 
-bool calendar(int num)
-{
-    if (num == 7 || num == 6)
-    return true;
-    else return false;
-}
-System.Console.Write("Input  number day of week : ");
-int number = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine($"today vacation? -> {calendar(number)}");
+// bool calendar(int num)
+// {
+//     if (num == 7 || num == 6)
+//     return true;
+//     else return false;
+// }
+// System.Console.Write("Input  number day of week : ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"today vacation? -> {calendar(number)}");
