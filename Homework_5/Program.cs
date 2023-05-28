1a98f0141a0a96585ac20b3dcd7ea1b3b3f6548e
valmar98@mail.ru <valmar98@mail.ru>
2023-05-28 11:03:57 +0300

    dd homework 7

--- Homework_5/Program.cs
+++ Homework_5/Program.cs
@@ -3,36 +3,36 @@
 // // 456 -> 5
 // // 782 -> 8
 // // 918 -> 1
-// int secondNumber(int num)
-// {
-//     if (num / 100 == 0)
-//         return num % 10;
-//     else
-//         num = num / 10;
-//        return num = num % 10;
-// }
-// System.Console.Write("Input  number typ (adc): ");
-// int number = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine($"second number is -> {secondNumber(number)}");
+int secondNumber(int num)
+{
+    if (num / 100 == 0)
+        return num % 10;
+    else
+        num = num / 10;
+       return num = num % 10;
+}
+System.Console.Write("Input  number typ (adc): ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"second number is -> {secondNumber(number)}");
 
 
 // // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-// // 645 -> 5
-// // 78 -> третьей цифры нет
-// // 32679 -> 6
-// int secondNumber(int num)
-// {
-//     if (num / 100 == 0)
-//     {
-//         System.Console.WriteLine("no third digit");
-//         return num =-1;
-//     }
-//     else
-//        return num = num % 10;
-// }
-// System.Console.Write("Input  number typ (adc): ");
-// int number = Convert.ToInt32(Console.ReadLine());
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+int secondNumber(int num)
+{
+    if (num / 100 == 0)
+    {
+        System.Console.WriteLine("no third digit");
+        return num =-1;
+    }
+    else
+       return num = num % 10;
+}
+System.Console.Write("Input  number typ (adc): ");
+int number = Convert.ToInt32(Console.ReadLine());
 
 // System.Console.WriteLine($"second number is -> {secondNumber(number)}");
 
@@ -42,12 +42,12 @@
 // // 7 -> да
 // // 1 -> нет
 
-// bool calendar(int num)
-// {
-//     if (num == 7 || num == 6)
-//     return true;
-//     else return false;
-// }
-// System.Console.Write("Input  number day of week : ");
-// int number = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine($"today vacation? -> {calendar(number)}");
+bool calendar(int num)
+{
+    if (num == 7 || num == 6)
+    return true;
+    else return false;
+}
+System.Console.Write("Input  number day of week : ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"today vacation? -> {calendar(number)}");
